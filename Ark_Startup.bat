3e1a7a86284849516d87ce572bf0531a1066112c
Grumbo <blackwolfsden@hotmail.com>
2025-11-18 01:27:56 -0700

    Update Ark_Startup.bat

--- Ark_Startup.bat
+++ Ark_Startup.bat
@@ -7,7 +7,7 @@ SET GAMEPORT=7777;
 SET QPORT=27015;
 SET RCONPORT=32330;
 SET PLAYERCNT=8;
-SET MULTIIP="173.47.92.3";
+SET MULTIIP="0.0.0.0";
 
 start ..\Ark_Server\ShooterGame\Binaries\Win64\ShooterGameServer.exe %MAPNAME%?ServerCrosshair=true?listen?SessionName=%SERVERNAME%?Port=%GAMEPORT%?QueryPort=%QPORT%?MaxPlayers=%PLAYERCNT%?-NoBattlEye
 
